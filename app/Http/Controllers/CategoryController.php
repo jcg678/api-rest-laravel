a7bcf3984f4991857755f973ee14eb65079e7963
jcg678 <jcg678@hotmail.es>
2019-12-15 19:23:52 +0100

    Guardar una categoria

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,6 +8,11 @@ use App\Category;
 
 class CategoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('api.auth',['except'=>['index','show']]);
+    }
+
     public function index(){
         $categories = Category::all();
 
@@ -32,6 +37,43 @@ class CategoryController extends Controller
                 'category'=>$category
             ];
         }
+        return response()->json($data, $data['code']);
+
+    }
+
+    public function store(Request $request){
+        $json = $request->input('json',null);
+        $params_array = json_decode($json, true);
+        $data = [
+            'code' => 400,
+            'status' => 'error',
+            'message' => 'No has envidado nada'
+        ];
+
+        if(!empty($params_array)) {
+        $validate = \Validator::make($params_array,[
+            'name' => 'required'
+        ]);
+
+            if ($validate->fails()) {
+                $data = [
+                    'code' => 400,
+                    'status' => 'error',
+                    'message' => 'no se ha guardado la categoria'
+                ];
+            } else {
+                $category = new Category;
+                $category->name = $params_array['name'];
+                $category->save();
+
+                $data = [
+                    'code' => 200,
+                    'status' => 'success',
+                    'category' => $category
+                ];
+            }
+        }
+
         return response()->json($data, $data['code']);
 
     }
